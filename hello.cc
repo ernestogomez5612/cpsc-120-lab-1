56d15b21174cac7df55e96e9d19eaceed439b1ad
ernestogomez5612 <143227310+ernestogomez5612@users.noreply.github.com>
2024-09-17 00:51:50 -0700

    Add files via upload

---
+++ hello.cc
@@ -0,0 +1,11 @@
+// Ernesto Gomez
+// ernestogomez5612@csu.fullerton.edu
+// @ernestogomez5612
+// Partners: @Amdinisi5
+
+#include <iostream>
+
+int main(int argc, const char *argv[]) {
+  std::cout << "Hello Ernesto!\n";
+  return 0;
+}
